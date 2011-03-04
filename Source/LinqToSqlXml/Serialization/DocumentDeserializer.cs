15e3eb34ce41d981fdb9543a55c5cb5206daec4e
Roger Alsing <roger.alsing@precio.se>
2011-03-04 11:11:22 +0100

    fixed anonymous type projections

--- Source/LinqToSqlXml/Serialization/DocumentDeserializer.cs
+++ Source/LinqToSqlXml/Serialization/DocumentDeserializer.cs
@@ -39,7 +39,7 @@ namespace LinqToSqlXml
             {
                 var elementExpectedType = typeof(object);
                 if (expectedType.IsGenericType)
-                    elementExpectedType = expectedType.GetGenericArguments()[1];
+                    elementExpectedType = expectedType.GetGenericArguments()[0];
 
                 return xml.Elements().Select(e => Deserialize(e, elementExpectedType)).ToList();
             }
@@ -55,7 +55,29 @@ namespace LinqToSqlXml
             if (type.GetConstructor(new Type[] { }) == null)
             {
                 //deserialize with ctor args
-                return null;
+                
+                var ctor = type.GetConstructors().First();
+                var ctorArgs = ctor.GetParameters();
+                var values = new object[ctorArgs.Length];
+                int i=0;
+                foreach (XElement xProperty in xml.Elements().Where(e => !e.Name.LocalName.StartsWith("__")))
+                {
+                    ParameterInfo parameter = ctorArgs.Where(p => p.Name == xProperty.Name).First();
+                    var expectedPropertyType = parameter.ParameterType;
+
+                    object parameterValue = Deserialize(xProperty, expectedPropertyType);
+                    if (parameterValue is List<object>)
+                    {
+                        parameterValue = DeserializeList2(parameterValue, expectedPropertyType);
+                    }
+                    values[i] = parameterValue;
+                    i++;
+                    //values[parameter.Name] = parameterValue;
+                }
+
+                object result = Activator.CreateInstance(type, values);
+
+                return result;
             }
             else
             {
@@ -109,6 +131,38 @@ namespace LinqToSqlXml
             {
                 property.SetValue(result, value, null);
             }
+        }
+
+        private static object DeserializeList2(object propertyValue, Type collectionType)
+        {
+            var list = (List<object>)propertyValue;
+
+            if (collectionType.IsInterface && collectionType.IsGenericType)
+            {
+                Type elementType = collectionType.GetGenericArguments().First();
+                Type concreteType = typeof(List<>).MakeGenericType(elementType);
+                var concreteList = (IList)Activator.CreateInstance(concreteType);
+                foreach (object item in list)
+                    concreteList.Add(item);
+
+                return concreteList;
+            }
+            else if (typeof(Array).IsAssignableFrom(collectionType))
+            {
+                int length = list.Count;
+                Array arr = null;
+                for (int i = 0; i < length; i++)
+                {
+                    arr.SetValue(list[i], i);
+                }
+                return arr;
+            }
+            else if (collectionType == typeof(object))
+            {
+                throw new Exception();
+            }
+
+            throw new NotSupportedException("");
         }
 
         private static object DeserializeDateTime(string propertyString)
